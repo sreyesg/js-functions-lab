e7b9957b6daf4b7004a7cc87735298defb04a8d5
sreyeg <santiagoreyes.g@gmail.com>
2024-06-29 15:01:00 -0700

    setup files

---
+++ script.js
@@ -0,0 +1,118 @@
+/*
+Exercise 1: maxOfTwoNumbers()
+
+In this exercise, create a function named maxOfTwoNumbers. 
+It should take two numbers as inputs and return the larger number. 
+If they're equal, return either one.
+
+Exercise 1 has been completed for you:
+*/
+
+const maxOfTwoNumbers = (x, y) => {
+    if (x >= y) {
+      return x;
+    } else {
+      return y;
+    }
+  }
+  
+//   console.log('Exercise 1 Result:', maxOfTwoNumbers(3, 9));
+  /*
+Exercise 2: isAdult()
+
+Write a function named isAdult. It should take an age (number) 
+and return 'Adult' if the age is 18 or over and 'Minor' otherwise.
+
+Example: isAdult(21) should return 'Adult'.
+
+Complete the exercise in the space below:
+*/
+
+const isAdult = (age) => {
+    if (age >=18){
+        return 'Adult'
+    }else {
+        return 'Minor'
+    }
+}
+// console.log('Exercise 2 Result:', isAdult(21));
+
+/*
+Exercise 3: isCharAVowel()
+
+Write a function named isCharAVowel that takes a single character as 
+an argument. It should return true if the character is a vowel and 
+false otherwise. For the purposes of this exercise, the character 
+y should not be considered a vowel.
+
+Example: isCharAVowel('a') should return true.
+
+Complete the exercise in the space below:
+*/
+const isCharAVowel = (char) => {
+    const lowercaseAlphabet = [
+        'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+        'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+    ];
+    char.toLowerCase()
+    console.log(char)
+    for (i=0; i<lowercaseAlphabet.length; i++){   
+        if (lowercaseAlphabet[i]===char.toLowerCase()) {
+            return true
+        }
+    }
+}
+// console.log(isCharAVowel("M"))
+// console.log('Exercise 3 Result:', isCharAVowel("a"));
+
+/*
+Exercise 4: generateEmail()
+
+Create a function named generateEmail. It should take two strings: 
+a name and a domain. It should return a simple email address.
+
+Example: generateEmail('johnsmith', 'example.com') 
+should return 'johnsmith@example.com'.
+
+Complete the exercise in the space below:
+*/
+
+const generateEmail = (name, domain) => {
+    return `${name}@${domain}`
+}
+
+// console.log('Exercise 4 Result:', generateEmail("johnsmith", "example.com"));
+/*
+Exercise 5: greetUser()
+
+Define a function called greetUser. It should take a name and a 
+time of day (morning, afternoon, evening) and return a personalized 
+greeting.
+
+Example: greetUser('Sam', 'morning') should return "Good morning, Sam!"
+
+Complete the exercise in the space below:
+*/
+
+const greetUser = (name, timeOfDay) => {
+    return `GOod ${timeOfDay}, ${name}`
+}
+
+// console.log('Exercise 5 Result:', greetUser("Sam", "morning"));
+
+/*
+Exercise 6: maxOfThree()
+
+Define a function, maxOfThree. It should accept three numbers 
+and return the largest among them.
+
+Example: maxOfThree(17, 4, 9) should return 17.
+
+Complete the exercise in the space below:
+*/
+const maxOfThree = (numA, numB, numC) => {
+    return Math.max(numA,numB,numC)
+}
+
+
+// console.log('Exercise 6 Result:', maxOfThree(5, 10, 8));
